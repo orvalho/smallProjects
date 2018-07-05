663ee8569d6883aa4e432384ebfd55d2f77d3b73
orvalho <rasa396@gmail.com>
2018-07-05 14:48:31 +0300

    typingEffect2 add word typewriter

--- typingEffect2/resources/main.js
+++ typingEffect2/resources/main.js
@@ -4,6 +4,8 @@ window.addEventListener('load', main);
 function main() {
   const WORDS = ["dream", "create", "inspire"];
   const SPEED_WRITE = 300;
+  const SPEED_CHANGE_WORD = 200;
+  const wordsLength = WORDS.length;
   let i = 0; // index of letter in a word
   let j = 0; // index of word in an array of words
   // select an element to write text to
@@ -17,6 +19,13 @@ function main() {
       element.textContent += WORDS[j].charAt(i);
       i++;
       setTimeout(typeWriter, SPEED_WRITE);
+    }
+    // go to next word
+    else if (j < wordsLength - 1) {
+      element.textContent = ""; // clear
+      j++; // next word
+      i = 0; // reset i
+      setTimeout(typeWriter, SPEED_CHANGE_WORD);
     }
   }
 }
