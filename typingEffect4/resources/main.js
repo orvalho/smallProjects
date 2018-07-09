eaf8e5b6b0392bc414441424e3db69f9338a19c2
orvalho <rasa396@gmail.com>
2018-07-09 19:24:25 +0300

    typingEffect4 add resetter

--- typingEffect4/resources/main.js
+++ typingEffect4/resources/main.js
@@ -5,6 +5,7 @@ const main = () => {
   const textLength = TEXT.length;
   const SPEED_START = 800;
   const SPEED_DELETE = 200;
+  const SPEED_RESET = 400;
   let i = textLength - 1; // index of letter in a word
 
   // select an element and write text to it
@@ -17,6 +18,12 @@ const main = () => {
       element.textContent = TEXT.slice(0, i);
       i--;
       setTimeout(typeWriter, SPEED_DELETE);
+    }
+    // reset and loop forever
+    else {
+      element.textContent = TEXT; // reset
+      i = textLength - 1; // reset
+      setTimeout(typeWriter, SPEED_RESET);
     }
   };
 
