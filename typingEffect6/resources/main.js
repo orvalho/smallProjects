4cac9d9801fab3dbf3e078224948b99349eb672d
orvalho <rasa396@gmail.com>
2018-07-10 16:28:13 +0300

    typingEffect6 write a word one letter at a time

--- typingEffect6/resources/main.js
+++ typingEffect6/resources/main.js
@@ -4,7 +4,10 @@ const main = () => {
   const WORDS = ["joy", "trust", "patience"];
   const wordsLength = WORDS.length;
 
+  const SPEED_WRITE = 150;
+
   let j = 0; // index of word in an array of words, while writing
+  let i = 0; // index of letter in a word, while writing
 
   const text = document.querySelector('#text');
 
@@ -19,6 +22,17 @@ const main = () => {
   j = 0; // reset j
 
   const tags = document.querySelectorAll('#text p');
+
+  const typeWriter = () => {
+    // write a word one letter at a time
+    if(i < WORDS[j].length) {
+      tags[j].textContent += WORDS[j].charAt(i);
+      i++;
+      setTimeout(typeWriter, SPEED_WRITE);
+    }
+  };
+
+  typeWriter();
 };
 
 window.addEventListener('load', main);
