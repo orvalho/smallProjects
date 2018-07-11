ae037e5e858e96ca1e2c9c2cf4d68d352d4e58d9
orvalho <rasa396@gmail.com>
2018-07-11 17:13:55 +0300

    typingEffect7 delete mistake one letter at a time

--- typingEffect7/resources/main.js
+++ typingEffect7/resources/main.js
@@ -7,10 +7,13 @@ const main = () => {
   const MISTAKE_AT = 4; // index
   const wordWithMistake = WORD.slice(0, MISTAKE_AT) + MISTAKE;
   const wordWithMistakeLength = wordWithMistake.length;
+  const wordCorrectPartLength = WORD.slice(0, MISTAKE_AT).length;
 
   let i = 0; // index of letter in a word with mistake, while writting
+  let k = wordWithMistakeLength - 1; // index of letter in a word with mistake, while deleting
 
   const SPEED_WRITE = 200;
+  const SPEED_DELETE = 500;
 
   // select an element to write text to
   const element = document.querySelector('#element');
@@ -21,6 +24,12 @@ const main = () => {
       element.textContent += wordWithMistake.charAt(i);
       i++;
       setTimeout(typeWriter, SPEED_WRITE);
+    }
+    // delete mistake one letter at a time
+    else if(k >= wordCorrectPartLength) {
+      element.textContent = element.textContent.slice(0, k);
+      k--;
+      setTimeout(typeWriter, SPEED_DELETE);
     }
   };
 
