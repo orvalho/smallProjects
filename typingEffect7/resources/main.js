13fc9e0b20a8f1657f00181894af8d04a992b352
orvalho <rasa396@gmail.com>
2018-07-11 17:23:51 +0300

    typingEffect7 write the rest of the word one letter at a time

--- typingEffect7/resources/main.js
+++ typingEffect7/resources/main.js
@@ -3,6 +3,7 @@
 const main = () => {
 
   const WORD = "sunshine";
+  const wordLength = WORD.length;
   const MISTAKE = "rmz";
   const MISTAKE_AT = 4; // index
   const wordWithMistake = WORD.slice(0, MISTAKE_AT) + MISTAKE;
@@ -11,6 +12,7 @@ const main = () => {
 
   let i = 0; // index of letter in a word with mistake, while writting
   let k = wordWithMistakeLength - 1; // index of letter in a word with mistake, while deleting
+  let j = MISTAKE_AT; // index of letter in a word after deleting mistake, while writting
 
   const SPEED_WRITE = 200;
   const SPEED_DELETE = 500;
@@ -30,6 +32,12 @@ const main = () => {
       element.textContent = element.textContent.slice(0, k);
       k--;
       setTimeout(typeWriter, SPEED_DELETE);
+    }
+    // write the rest of the word one letter at a time
+    else if(j < wordLength) {
+      element.textContent += WORD.charAt(j);
+      j++;
+      setTimeout(typeWriter, SPEED_WRITE);
     }
   };
 
