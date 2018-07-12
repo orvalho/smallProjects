5db0c585d5ca0906b7421b48fb4adfa32457e101
orvalho <rasa396@gmail.com>
2018-07-12 17:41:39 +0300

    progressBar add progress bar

---
+++ progressBar/resources/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const progressBar = () => {
+
+  const percent = document.getElementById('percent');
+  const outer = document.getElementById('outer');
+  const inner = document.getElementById('inner');
+
+  const outerWidth = outer.offsetWidth;
+  let innerWidth = 0;
+  let percentValue = 0;
+
+  const SPEED = 1;
+
+  const frame = () => {
+    if(innerWidth < outerWidth) {
+      innerWidth++;
+      inner.style.width = `${innerWidth}px`;
+      percentValue = Math.round(innerWidth / outerWidth * 100);
+      percent.textContent = `${percentValue}%`;
+      setTimeout(frame, SPEED);
+    }
+  };
+
+  frame();
+};
+
+window.addEventListener('load', progressBar);
