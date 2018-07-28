37f16b6943927f26f261777e83d49a6a0c5c44cb
orvalho <rasa396@gmail.com>
2018-07-28 14:25:00 +0300

    loginForm close modal login form

--- loginForm/js/scripts.js
+++ loginForm/js/scripts.js
@@ -2,6 +2,7 @@
 
 window.addEventListener('load', () => {
   const openModalLoginButton = document.getElementById('openModalLoginButton');
+  const closeModalLoginButton = document.getElementById('closeModalLoginButton');
   const modal = document.getElementById('modal');
 
   // open modal login
@@ -10,4 +11,19 @@ window.addEventListener('load', () => {
     openModalLoginButton.style.display = 'none';
   };
   openModalLoginButton.addEventListener('click', openModalLogin);
+
+  // close modal login when user clicks close button
+  const closeModalLogin = () => {
+    modal.style.display = 'none';
+    openModalLoginButton.style.display = 'block';
+  };
+  closeModalLoginButton.addEventListener('click', closeModalLogin);
+
+  // close modal login when user clicks anywhere outside of the form
+  document.addEventListener('click', (e) => {
+    if(e.target === modal) {
+      modal.style.display = 'none';
+      openModalLoginButton.style.display = 'block';
+    }
+  });
 });
